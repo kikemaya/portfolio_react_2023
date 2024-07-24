c9a36e17757223dfd67753253ffa9135ac6108ea
kikemaya <enrique.mg.cd@gmail.com>
2024-07-24 11:07:27 -0600

    image optimizer, added

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,32 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vitest/config";
 
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/",
-	plugins: [react()],
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./src/setupTests.ts",
-		css: true,
-		reporters: ["verbose"],
-		coverage: {
-			reporter: ["text", "json", "html"],
-			include: ["src/**/*"],
-			exclude: [],
-		},
-	},
+  base: "/",
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpg: {
+        quality: 10,
+      },
+      png: {
+        quality: 10,
+      },
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    css: true,
+    reporters: ["verbose"],
+    coverage: {
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*"],
+      exclude: [],
+    },
+  },
 });
